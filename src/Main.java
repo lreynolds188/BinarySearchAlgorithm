1407bf51c96f6146e1389617a6274733d7f9846f
lreynolds <lreynolds188@gmail.com>
2018-03-12 21:17:33 +1000

    - Added plot functionality - Renamed Main to Search - Renamed Tests to Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,60 @@
-public class Main {
+import org.jfree.ui.RefineryUtilities;
 
-    public void BinarySearch(){
-        
+import java.util.Random;
+
+public class Main extends Search{
+
+    static int[] arr;
+    static Random rand = new Random();
+    static long startTimeNano, endTimeNano, totalTimeNano;
+    static long startTime, endTime, totalTime;
+
+    public static void main(String[] args){
+        ArraySearchTest();
+        PlotGraph();
     }
 
+    public static void ArraySearchTest(){
+        for (int size = 1000; size <= 20000; size += 1000){
+            long[] timeArr = new long[20];
+            long[] timeArrNano = new long[20];
+            for (int i = 0; i < 20; i++) {
+                arr = CreateArray(size);
+                arr = SortArray(arr);
+                System.out.print("Array search size: " + size);
+
+                int key = rand.nextInt(size);
+                startTime = System.currentTimeMillis();
+                startTimeNano = System.nanoTime();
+                int searchRes = BinarySearch(arr, key);
+                endTime = System.currentTimeMillis();
+                endTimeNano = System.nanoTime();
+
+                System.out.print("\tSearched for " + key + "... ");
+                if (searchRes != -1) {
+                    System.out.print("Found match at index " + searchRes + "\n");
+                } else {
+                    System.out.print("Did not find a match.\n");
+                }
+                timeArrNano[i] = endTimeNano - startTimeNano;
+                timeArr[i] = endTime - startTime;
+
+            }
+            for (int i = 0; i < timeArr.length; i++){
+                totalTime += timeArr[i];
+                totalTimeNano += timeArrNano[i];
+            }
+            System.out.print("Average test time was: " + (totalTime / 20) + "ms / " + (totalTimeNano / 20) + "ns\n");
+        }
+    }
+
+    public static void PlotGraph(){
+        Plot chart = new Plot(
+                "School Vs Years" ,
+                "Numer of Schools vs years");
+
+        chart.pack( );
+        RefineryUtilities.centerFrameOnScreen( chart );
+        chart.setVisible( true );
+    }
 }
---
+++ src/Search.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+public class Search {
+
+    /**
+     *  Implements non-recursive binary search of an int array.
+     *
+     *  @param arr - an array containing values to be searched.
+     *  @param key - the value to be searched for.
+     *  @return
+     */
+    public static int BinarySearch(int[] arr, int key){
+        int curIndex;
+        int range = arr.length;
+        for (int i = 0; i < range; i++){
+            curIndex = (i + arr.length)/2;
+            if (arr[curIndex] == key){
+                return curIndex;
+            } else if (key < arr[curIndex]){
+                range = curIndex-1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Creates an int array of the designated size.
+     *
+     * @param size
+     * @return
+     */
+    public static int[] CreateArray(int size){
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++){
+            arr[i] = rand.nextInt(size);
+        }
+        return arr;
+    }
+
+    /**
+     * Implements sorting algorithm to designated int array.
+     *
+     * @param arr - array containing values to be sorted.
+     * @return
+     */
+    public static int[] SortArray(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++){
+            for(int j = 1; j < (n-i); j++){
+                if(arr[j - 1] > arr[j]){
+                    //swap elements
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+
+    }
+
+}
